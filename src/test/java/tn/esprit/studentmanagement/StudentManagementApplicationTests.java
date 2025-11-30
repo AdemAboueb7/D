6107a57268c4b6c079a35cc3edb53a5a5cafa411
adem_aboueb <adem.aboueb@esprit.tn>
2025-11-30 22:06:40 +0100

    v1

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -2,12 +2,15 @@ package tn.esprit.studentmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // ← LIGNE TRÈS IMPORTANTE
 class StudentManagementApplicationTests {
 
     @Test
     void contextLoads() {
+        // Ce test vérifie simplement que le contexte Spring se charge correctement
     }
 
 }
