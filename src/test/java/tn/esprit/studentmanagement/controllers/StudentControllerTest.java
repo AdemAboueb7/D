e1ad2dd54ef221786253decc612a17f04d18b8d8
adem_aboueb <adem.aboueb@esprit.tn>
2025-12-06 14:22:02 +0100

    ...

--- src/test/java/tn/esprit/studentmanagement/controllers/StudentControllerTest.java
+++ src/test/java/tn/esprit/studentmanagement/controllers/StudentControllerTest.java
@@ -3,7 +3,7 @@ package tn.esprit.studentmanagement.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.MockBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import com.fasterxml.jackson.databind.ObjectMapper;
